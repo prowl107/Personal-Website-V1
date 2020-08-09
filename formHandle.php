d0bc363da89133e5c14086a0a88541140fd5d8e6
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-09 14:36:46 -0400

    Contact form fix
    Fixed contact form HTML and PHP files

--- formHandle.php
+++ formHandle.php
@@ -2,9 +2,10 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];  
-$subject = "Contact Form Submission: ${name} , ${email}";
-$email_reciever = "pxqsaklo@prowl107.com";
+$subject = $_POST["subject"];
+$header = "Contact Form Submission: ${name} , ${email}";
+$recipient = "pxqsaklo@prowl107.com";
 
-mail($email_reciever,$subject,$message);
+mail($recipient,$subject,$message,$header);
 ?>
 

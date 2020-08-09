ab517118f5f0fa4e2c63364a2361a2f9190eeb1f
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-09 02:58:24 -0400

    Fix Image locations

--- formHandle.php
+++ formHandle.php
@@ -2,7 +2,7 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];  
-$subject = "New form submission from: ${name} , ${email}";
+$subject = "Contact Form Submission: ${name} , ${email}";
 $email_reciever = "pxqsaklo@prowl107.com";
 
 mail($email_reciever,$subject,$message);

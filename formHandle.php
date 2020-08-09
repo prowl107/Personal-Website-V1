45ec993ecbab1495e953ec21cb1366d686c80efc
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-09 02:48:10 -0400

    Index.html and CSS Updates

--- formHandle.php
+++ formHandle.php
@@ -2,8 +2,8 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];  
-$subject = "New form submission from: ${name}, ${email}";
-$email_reciever = "osborm10@my.erau.edu";
+$subject = "New form submission from: ${name} , ${email}";
+$email_reciever = "pxqsaklo@prowl107.com";
 
 mail($email_reciever,$subject,$message);
 ?>

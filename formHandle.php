82ba27e733f2107a2105dccd0869bb257baf3f20
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-09 15:36:27 -0400

    Update contact.html, trial formhandle.php script

--- formHandle.php
+++ formHandle.php
@@ -1,10 +1,22 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];  
-$subject = $_POST["subject"];
-$header = "Contact Form Submission: ${name} , ${email}";
-$recipient = "pxqsaklo@prowl107.com";
+if(isset($_POST['send'])){
+    foreach( $_POST as $fields ) {
+        if( !empty($_POST[$fields])){
+         $postCheck = true;
+        } else {
+            $postCheck = false;
+            echo "Failed Validation - Field Empty";
+        }
+    }
+}
+
+$postCheck = false;
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];  
+$header = $_POST['subject'];
+$subject = 'Contact Form Submission: ${name} , ${email}';
+$recipient = 'pxqsaklo@prowl107.com';
 
 mail($recipient,$subject,$message,$header);
 ?>

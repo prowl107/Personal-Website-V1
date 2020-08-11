8c0f17cddee8ddcebcdcebfd5bf0f83b95559bb0
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-10 22:50:25 -0400

    Added client side form validation
    Client side form validation with javascript and abide library

--- js/app.js
+++ js/app.js
@@ -1,49 +1,13 @@
 $(document).foundation()
 
-// window.scroll(0, 1);
+//Simple client side form validation:
+const form = document.querySelector('form');
+var fieldCheck = false
 
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function () { myFunction() };
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//     navbar.classList.add("is-anchored is-at-bottom")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-// document.getElementById("projectAnchor1").addEventListener("click", projectAnchor_toggle1);
-// function projectAnchor_toggle1() {
-//   if (document.getElementById("projectAnchor1").innerHTML == "Expand Description") {
-//     document.getElementById("projectAnchor1").innerHTML = "Collapse Description";
-//   } else {
-//     document.getElementById("projectAnchor1").innerHTML = "Expand Description";
-//   }
-// }
-
-// document.getElementById("projectAnchor2").addEventListener("click", projectAnchor_toggle2);
-// function projectAnchor_toggle2() {
-//   if (document.getElementById("projectAnchor2").innerHTML == "Expand Description") {
-//     document.getElementById("projectAnchor2").innerHTML = "Collapse Description";
-//   } else {
-//     document.getElementById("projectAnchor2").innerHTML = "Expand Description";
-//   }
-// }
-
-// document.getElementById("projectAnchor3").addEventListener("click", projectAnchor_toggle3);
-// function projectAnchor_toggle3() {
-//   if (document.getElementById("projectAnchor3").innerHTML == "Expand Description") {
-//     document.getElementById("projectAnchor3").innerHTML = "Collapse Description";
-//   } else {
-//     document.getElementById("projectAnchor3").innerHTML = "Expand Description";
-//   }
-// }
+function updateValue(e) {
+    if (form.checkValidity()) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}

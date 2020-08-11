8d6e8b4a636ea3786f076337778d69cecc4a6bfe
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-10 23:00:15 -0400

    Added event listener

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ $(document).foundation()
 //Simple client side form validation:
 const form = document.querySelector('form');
 var button = document.getElementById('form_send');
-var fieldCheck = false
+
+form.addEventListener('input',updateValue);
 
 function updateValue(e) {
     if (form.checkValidity()) {

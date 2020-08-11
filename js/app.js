7d81bad9f627bb151b2f01e51941ece7b13a9938
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-11 03:42:15 -0400

    Mobile response tweaks and styling

--- js/app.js
+++ js/app.js
@@ -13,3 +13,27 @@ if (document.URL.includes('contact.html')) {
         }
     }
 }
+
+//Featured project image filter
+if (document.URL.includes('index.html')) {
+    var button1 = document.getElementById('featured-button-1');
+    var button2 = document.getElementById('featured-button-2');
+
+    button1.onmouseover = function () {
+        document.getElementById('featured-image-1').style.filter = "contrast(130%)";
+        document.getElementById('featured-image-1').style.transition = "all 0.25s ease-in-out";
+        button1.onmouseleave = function () {
+            document.getElementById('featured-image-1').style.filter = "contrast(100%)";
+            document.getElementById('featured-image-1').style.transition = "all .25s ease-in-out";
+        }
+    }
+
+    button2.onmouseover = function () {
+        document.getElementById('featured-image-2').style.filter = "contrast(120%)";
+        document.getElementById('featured-image-2').style.transition = "all 0.25s ease-in-out";
+        button2.onmouseout = function () {
+            document.getElementById('featured-image-2').style.filter = "contrast(100%)";
+            document.getElementById('featured-image-2').style.transition = "all .25s ease-in-out";
+        }
+    }
+}

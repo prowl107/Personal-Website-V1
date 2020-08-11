4c0dab4e964d5ca014f4ad32ba5c518e43e7c3c3
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-10 22:56:50 -0400

    Fixed missing button error

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).foundation()
 
 //Simple client side form validation:
 const form = document.querySelector('form');
+var button = document.getElementById('form_send');
 var fieldCheck = false
 
 function updateValue(e) {

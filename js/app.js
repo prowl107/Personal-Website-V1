8d2c86d039adac6716eff8ef079afec55686391a
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-08-10 23:12:25 -0400

    Added check for contact.html page
    Should only run javascript function on the contact.html page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(document).foundation()
 
 //Simple client side form validation:
-const form = document.querySelector('form');
-var button = document.getElementById('form_send');
-
-form.addEventListener('input',updateValue);
-
-function updateValue(e) {
-    if (form.checkValidity()) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
+if (document.URL.includes('contact.html')) {
+    const form = document.querySelector('form');
+    var button = document.getElementById('form_send');
+    form.addEventListener('input', updateValue);
+    function updateValue(e) {
+        if (form.checkValidity()) {
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
     }
 }

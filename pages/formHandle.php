a0f6b6c4200534979c573bf01fd69e6cb17204e6
prowl107 <45919564+prowl107@users.noreply.github.com>
2020-09-27 15:29:35 -0400

    Form handle update and small projects tweak

--- pages/formHandle.php
+++ pages/formHandle.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv = "refresh" content = "7; url = https://www.milesosborne.com" />
+    <meta http-equiv = "refresh" content = "4; url = https://www.milesosborne.com" />
     <title>Thank You</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
